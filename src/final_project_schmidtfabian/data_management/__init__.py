e0d239155af94af9481c7baf462c67e15da09e1d
Fabian Schmidt <s55fschm@uni-bonn.de>
2024-03-03 21:44:04 +0100

    Fix __init__.py files due to deleting Python example from the project.

--- src/final_project_schmidtfabian/data_management/__init__.py
+++ src/final_project_schmidtfabian/data_management/__init__.py
@@ -1,5 +1,1 @@
 """Functions for managing data."""
-
-from final_project_schmidtfabian.data_management.clean_data import clean_data
-
-__all__ = [clean_data]
--- src/final_project_schmidtfabian/final/__init__.py
+++ src/final_project_schmidtfabian/final/__init__.py
@@ -1,5 +1,1 @@
 """Functions for formatting results."""
-
-from final_project_schmidtfabian.final.plot import plot_regression_by_age
-
-__all__ = [plot_regression_by_age]
