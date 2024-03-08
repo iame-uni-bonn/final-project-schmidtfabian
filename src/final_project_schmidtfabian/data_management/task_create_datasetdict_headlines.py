fa2ba745442d7a95704b8ab190a7dbb070be5605
Fabian Schmidt <s55fschm@uni-bonn.de>
2024-03-08 23:07:15 +0100

    Modify products in task_create_datasetdict_headlines.py such that tasks are executed in the correct order.

--- src/final_project_schmidtfabian/data_management/task_create_datasetdict_headlines.py
+++ src/final_project_schmidtfabian/data_management/task_create_datasetdict_headlines.py
@@ -10,13 +10,13 @@ from final_project_schmidtfabian.config import SRC, BLD
 from final_project_schmidtfabian.data_management.create_datasetdict import create_datasetdict
 
 create_datasetdict_deps = {
-    "data handlabeled": SRC / "final_project_schmidtfabian" / "data" / "headlinesdatasetfinetuninghandlabeled.xlsx",
+    "data handlabeled": SRC / "data" / "headlinesdatasetfinetuninghandlabeled.xlsx",
     "scripts" : Path("create_datasetdict.py")
 }
 
 def task_create_datasetdict_for_training(
         depends_on = create_datasetdict_deps,
-        produces = BLD / "data" / "datasetdict_headlines"
+        produces = BLD/"data"/"dataset_dict.json"
         ):
     handlabeled_dataset_headlines = pd.read_excel(depends_on["data handlabeled"],sheet_name=0, usecols="A,B",skiprows=1392, header=None, names=["text","label"], nrows=300)
     label_mapping = {
@@ -26,4 +26,4 @@ def task_create_datasetdict_for_training(
     }
     handlabeled_dataset_headlines['label'] = handlabeled_dataset_headlines['label'].map(label_mapping)
     datasetdict_headlines = create_datasetdict(handlabeled_dataset_headlines)
-    datasetdict_headlines.save_to_disk(produces)
+    datasetdict_headlines.save_to_disk(BLD/"data")
